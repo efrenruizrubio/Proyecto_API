dcf6c6e11ba2752fff5744a29c73746295bc05b0
efren282 <efren282@outlook.es>
2021-11-05 02:39:03 -0600

    Update register_factura.php

--- facturas/register_factura.php
+++ facturas/register_factura.php
@@ -3,14 +3,16 @@ require "../includes/connection.php";
 require "../includes/requester.php";
 $params = Requester::getParams();
 
-$query = "INSERT INTO factura (RFC_Exp, RFC_Rec, regimen, impuestos, 
-            cond_pago, metodo_pago) VALUES(
+$query = "INSERT INTO factura (RFC_Exp, RFC_Rec, regimen, impuestos, cond_pago, metodo_pago) VALUES(
   '$params->rfc_exp',
   '$params->rfc_rec',
   '$params->regimen',
   '$params->impuestos',
   '$params->cond_pago',
-  '$params->metodo_pago,')";
+  '$params->metodo_pago')";
+
+echo $query;
+$db->showError();
 
 $result = $db->insert($query);
 Requester::endRequest('correct');
