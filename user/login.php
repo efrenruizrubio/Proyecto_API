b560a2cff7d03276bd022527d3adfc66a8ee05a3
efren282 <efren282@outlook.es>
2021-11-05 00:50:52 -0600

    Actualización del inicio de sesión

--- user/login.php
+++ user/login.php
@@ -1,25 +1,26 @@
 <?php
-  /* session_start();
-  $sesion=session_id(); */
+  session_start();
+  $sesion=session_id();
   require "../includes/connection.php";
   require "../includes/requester.php";
   $params = Requester::getParams();
-  $query = "SELECT contra FROM usuario WHERE username = '$params->usuario'";
+  $query = "SELECT id_usuario,contra FROM usuario WHERE username = '$params->usuario'";
 
   $row = $db->fetchRow($query);
   if(!$row){
-    /* session_unset(); */
+    session_unset();
     Requester::endRequest("not-found");
   }
 
-  if($row->contra == $params->contra){
-    $flag = true;
-    /* $_SESSION['id'] = $row->id_usuario; */
+  $flag = password_verify($params->contra, $row->contra);
+  if($flag){
+    $_SESSION['id'] = $row->id_usuario;
   }else{
-    $flag = false;
-    /* session_unset(); */
+    session_unset();
   }
 
-  $msg = $flag == false ? 'incorrect' : 'correct';
-  Requester::endRequest($msg);
+  $msg = $flag === false ? 'incorrect' : 'correct';
+  Requester::endRequest($msg, array(
+    'id' => $sesion
+  ));
 ?>
