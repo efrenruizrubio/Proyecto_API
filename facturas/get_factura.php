9141fa5437e199edf91f3b196ae842b54b415f16
efren282 <efren282@outlook.es>
2021-11-07 16:21:54 -0600

    Cambios en la estructura de la base de datos
    Se eliminó la tabla producto y se asignaron sus campos a la tabla Factura, el php refleja el cambio del mismo

--- facturas/get_factura.php
+++ facturas/get_factura.php
@@ -3,8 +3,6 @@ require "../includes/connection.php";
 require "../includes/requester.php";
 $params = Requester::getParams();
 
-$params = (int) $params;
-
-$query = "SELECT * FROM factura WHERE id_factura = $params";
+$query = "SELECT * FROM factura WHERE id_factura = $params->id";
 $prod = $db->fetchAll($query);
 echo json_encode($prod);
--- facturas/register_factura.php
+++ facturas/register_factura.php
@@ -7,15 +7,19 @@ session_id($params->id);
 session_start();
 $id=$_SESSION['id'];
 
-$query = "INSERT INTO factura (RFC_Exp, RFC_Rec, regimen, impuestos, cond_pago, metodo_pago, fk_id_usuario) VALUES(
+$query = "INSERT INTO factura (RFC_Exp, RFC_Rec, regimen, impuestos, cond_pago, metodo_pago, descripcion_producto, cantidad, valor_unitario, fk_id_usuario) VALUES(
   '$params->rfc_exp',
   '$params->rfc_rec',
   '$params->regimen',
   '$params->impuestos',
   '$params->cond_pago',
   '$params->metodo_pago',
+  '$params->descripcion',
+  '$params->cantidad',
+  '$params->valor',
   '$id')";
 
+echo $query;
 $result = $db->insert($query);
 //Requester::endRequest('correct');
 Requester::endRequest('correct', array(
