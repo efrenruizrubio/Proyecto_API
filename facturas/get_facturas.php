420836ed3637412714e904e80498f8d0d6f92eff
efren282 <efren282@outlook.es>
2021-12-01 05:32:37 -0600

    Cambios finales

--- facturas/get_facturas.php
+++ facturas/get_facturas.php
@@ -7,7 +7,7 @@ session_id($params->id);
 session_start();
 $id=$_SESSION['id'];
 
-$query = "SELECT id_factura, RFC_Exp, RFC_Rec, regimen, impuestos, cond_pago, metodo_pago, fecha 
+$query = "SELECT id_factura, nombre_empresa, nombre_facturador, nombre_cliente, regimen, impuestos, cond_pago, metodo_pago, fecha 
 FROM Factura WHERE fk_id_usuario = $id";
 $facturas = $db->fetchAll($query);
 echo json_encode($facturas);
--- facturas/register_factura.php
+++ facturas/register_factura.php
@@ -7,7 +7,7 @@ session_id($params->id);
 session_start();
 $id=$_SESSION['id'];
 
-$query = "INSERT INTO factura (RFC_Exp, RFC_Rec, regimen, impuestos, cond_pago, metodo_pago, descripcion_producto, cantidad, valor_unitario, fk_id_usuario) VALUES(
+$query = "INSERT INTO factura (RFC_Exp, RFC_Rec, regimen, impuestos, cond_pago, metodo_pago, descripcion_producto, cantidad, valor_unitario, fk_id_usuario, nombre_empresa, direccion_empresa, ciudad_estado, zip_empresa, telefono_empresa, nombre_cliente, direccion_cliente, ciudad_cliente, zip_cliente, nombre_facturador) VALUES(
   '$params->rfc_exp',
   '$params->rfc_rec',
   '$params->regimen',
@@ -17,8 +17,18 @@ $query = "INSERT INTO factura (RFC_Exp, RFC_Rec, regimen, impuestos, cond_pago, 
   '$params->descripcion',
   '$params->cantidad',
   '$params->valor',
-  '$id')";
-  
+  '$id',
+  '$params->nombreEmpresa',
+  '$params->direccionEmpresa',
+  '$params->ciudadEstadoEmpresa',
+  '$params->codigoPostalEmpresa',
+  '$params->telefonoEmpresa',
+  '$params->nombreCliente',
+  '$params->direccionCliente',
+  '$params->ciudadEstadoCliente',
+  '$params->codigoPostalCliente',
+  '$params->nombreFacturador')";
+
 $result = $db->insert($query);
 Requester::endRequest('correct', array(
   'result' => $result
